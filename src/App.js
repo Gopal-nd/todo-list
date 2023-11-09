64fb31af97c9c9aea392e187167c42824ebb0f2a
Gopal N D <gopalhelpyou@gmail.com>
2023-11-09 21:18:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [list, setlist] = useState([]);
+  const [task, settask] = useState();
+  // const [status, setStatus] = useState(false);
+  const addToList =()=>{
+    const tasklist ={
+      id:list.length ===0 ?1 :list[list.length - 1].id + 1,
+      taskname:task,
+      completed:false,
+    }
+    setlist([...list,tasklist])
+  }
+  const deleteTask =(id)=>{
+    const newList = list.filter((list)=>{
+      return list.id!==id
+    })
+    setlist(newList);
+  }
+  const done = (id) =>{
+    setlist(list.map((tasklist)=>{
+      if(tasklist.id===id){
+        return {...tasklist,completed:true}
+      }else{
+        return task
+      }
+    })
+    )
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="head">
+      <input className='button' type="text" onChange={(e)=>settask(e.target.value)} />
+      <button className='button' onClick={addToList}>ADD TASK</button>
+    </div>
+    <div className="lists">
+    {
+      list.map((task)=>{
+        return (<div style={{backgroundColor:task.completed?"green":"white"}}>
+        <h1 className='text'>{task.taskname}</h1>
+        <button className='qw' onClick={()=>done(task.id)}>Completed</button>
+        <button className='qw' onClick={()=>deleteTask(task.id)}>X</button>
+        </div>)
+      })
+    }
+    </div>
     </div>
   );
 }
